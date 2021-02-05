2dae4b2bef4ea0a4b024ca1eed343a191884083a
sparsh1305 <70315426+sparsh1305@users.noreply.github.com>
2021-02-05 16:12:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: 'ODA3MTQ5MTM0MjA5MDg5NTM3.YBzyNQ.Ejt16xqgf73zSqKnE0-Q5nSOKJo',
+    token: 'ODA3MTQ5MTM0MjA5MDg5NTM3.YBzyNQ.B9HOgVcF-kOg-Yy0y0hj8g1unCs',
     prefix: '&'
 }
 
